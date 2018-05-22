bfd05fcbcd8f7770693cfe8be9d1a2de3565d2b4
vcroft <vincent.croft@cern.ch>
2018-05-22 18:05:02 +0200

    P-value implementation

--- histimator/estimator.py
+++ histimator/estimator.py
@@ -12,7 +12,7 @@ from .models import HistiModel
 from .pdfs import cpoisson
 # from scipy.stats import poisson
 from .util import FakeFuncCode
-from iminuit import describe
+from iminuit import Minuit, describe
 from iminuit.util import make_func_code
 
 
@@ -61,16 +61,66 @@ class BinnedLH(object):
         for par in parameters.keys():
             if "syst" in par.lower():
                 constraints.append(parameters[par])
-        constraint = st.norm(1., 1.).logpdf(np.asarray(constraints)).prod()
+        constraint = np.asarray([st.norm(con, 1.).pdf(0) for con in constraints]).prod()
         if constraint <= 0. or isNaN(constraint):
-            constraint = 0.
+            constraint = 1.
         h_meas = self.h
         if self.extended:
             return -(cpoisson.logpdf(self.N, h_pred.sum()) +
-                     cpoisson.logpdf(h_meas, h_pred).sum() + constraint)
+                     cpoisson.logpdf(h_meas, h_pred).sum() + np.log(constraint))
         else:
-            return -cpoisson.logpdf(h_meas, h_pred).sum() - constraint
+            return -cpoisson.logpdf(h_meas, h_pred).sum() - np.log(constraint)
 
+
+class ProfileLH:
+    def __init__(self, model, data=None, bins=40, weights=None,
+                 weighterrors=None, bound=None,
+                 badvalue=1000000, extended=False,
+                 use_w2=False, nint_subdiv=1, minimiser = 'minuit'):
+        if isinstance(model, HistiModel):
+            self.pdf = model.pdf
+            self.binedges = model.binedges
+            self.func_code = FakeFuncCode(self.pdf, dock=True)
+            self.parameters = model.Parameters()
+        else:
+            print("ERROR model should be an instance of HistiModels")
+        self.model = model
+        self.minimiser = minimiser
+        if hasattr(model, "data"):
+            self.data = model.data
+            if self.data is None:
+                print("error: data is None, please feed the model with data")
+            else:
+                self.h = np.asarray(self.data)
+                self.binned_data = data
+                self.N = self.h.sum()
+        else:
+            print("error: model has no attribute data")
+
+
+
+        self.params = model.Parameters()
+        self.observables = model.pois.keys()
+
+        pdf_sig = [i for i in describe(self.pdf) if i in model.pois.keys()]
+        self.func_code = make_func_code(pdf_sig)
+        self.func_defaults = None
+
+
+    def __call__(self, *arg):
+        pois = {key:arg[i] for i,key in enumerate(self.observables)}
+        self.constrained_params = self.model.FixedParameters(pois)
+
+        unconstrainedLH = BinnedLH(self.model)
+        minimiser = Minuit(unconstrainedLH, **self.params)
+        minimiser.migrad()
+        unconstrainedBestFit = minimiser.get_fmin().fval
+
+        constrainedLH = BinnedLH(self.model)
+        newminimiser = Minuit(constrainedLH, **self.constrained_params)
+        newminimiser.migrad()
+        constrainedBestFit = newminimiser.get_fmin().fval
+        return constrainedBestFit - unconstrainedBestFit
 
 def isNaN(num):
     return num != num
--- histimator/statistics.py
+++ histimator/statistics.py
@@ -1,35 +1,24 @@
 from iminuit import Minuit, describe
 from iminuit.util import make_func_code
-from .estimator import BinnedLH
+from .estimator import ProfileLH
 
 class ProfileLikelihoodCalculator:
     def __init__(self, model):
         self.model = model
-        self.pdf = model.pdf
-        self.data = model.data
-        pdf_sig = [i for i in describe(self.pdf) if i in self.model.pois.keys()]
-        self.func_code = make_func_code(pdf_sig)
-        self.func_defaults = None
+        self.test_stat = None
+        self.best_fit = []
 
+    def test_statistic(self): 
+        params = self.model.Observables()
+        plh = ProfileLH(self.model)
+        self.test_stat = plh
+        min = Minuit(plh, **params)
+        min.migrad()
+        self.best_fit = min.values
 
-    def __call__(self, *arg):
-        print 'args are',arg
-        params = self.model.Parameters()
-        unconstrainedLH = BinnedLH(self.model)
-        minimiser = Minuit(unconstrainedLH, **params)
-        minimiser.migrad()
-        unconstrainedBestFit = minimiser.get_fmin().fval
-        print "unconstrainted ", unconstrainedBestFit
-        pois = {}
-        for i,key in enumerate(self.model.pois.keys()):
-            pois[key] = arg[i]
-        print 'pois are', pois
-        print 'the argument is', arg
-        constrained_params = self.model.FixedParameters(pois)
-        print 'with parameters', constrained_params
-        constrainedLH = BinnedLH(self.model)
-        newminimiser = Minuit(constrainedLH, **constrained_params)
-        newminimiser.migrad()
-        constrainedBestFit = newminimiser.get_fmin().fval
-        print 'constrained',constrainedBestFit
-        return constrainedBestFit - unconstrainedBestFit
+    def p_value(self):
+        if self.test_stat is None:
+            self.test_statistic()
+        values = self.best_fit.values()
+        return 2*self.test_stat(*values)
+       
